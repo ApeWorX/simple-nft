ca93422c41bae7c7226df0daeca2956ead9aea2b
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-03-01 13:50:17 -0800

    feat: add code for merkle root and some deployment - WIP

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,10 @@
+from readline import append_history_file
 import click
 from ape.cli import network_option
-
+from ape import project
+from ape import accounts
+from trie import smt
+from pathlib import Path
 
 @click.group(short_help="Deploy the project")
 def cli():
@@ -16,10 +20,26 @@ def assets():
 @network_option()
 @click.argument("artist", ...)  # `AddressType`
 def nft(network, artist):
-    ... # `ApePiece.vy` deploy process to `network` here
+    with networks.ethereum.mainnet.use_provider(network):
+        a = accounts.load(artist)
+        piece = artist.deploy(project.ApePiece)
 
 
 @cli.command()
 @network_option()
 def auction(network):
+    with networks.ethereum.mainnet.use_provider(network):
+        pass
     ... # `BuyApe.vy` deploy process to `network` here
+
+
+@cli.command()
+def merkle_root(filename):
+    TRUE_NODE = b"\x00" * 31 + b"\x01"
+    FALSE_NODE = b"\x00" * 32
+    addresses = Path(filename).read_text().split("\n")
+    tree = smt.SparseMerkleTree(20, FALSE_NODE)
+    for address in addresses:
+        tree.set(to_bytes(hexstr=address), TRUE_NODE)
+    return tree.root_hash
+    # calculate merkle root with input file here
