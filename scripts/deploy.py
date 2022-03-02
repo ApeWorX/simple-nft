576c88c9dac9aecac60bb90da017d06c6106bb14
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-03-02 12:28:50 -0800

    feat: added get-proof method to merkle.py and linting

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,12 @@
-from readline import append_history_file
-import click
-from ape.cli import network_option, NetworkBoundCommand
-from ape import project
-from ape import accounts
-from trie import smt
 from pathlib import Path
+from readline import append_history_file
+
+import click
+from ape import accounts, project
+from ape.cli import NetworkBoundCommand, network_option
+from eth_utils import to_bytes
+from trie import smt
+
 
 @click.group(short_help="Deploy the project")
 def cli():
@@ -13,12 +15,12 @@ def cli():
 
 @cli.command()
 def assets():
-    ... # Upload all assets and generated JSON files from `scripts/data` here
+    pass  # Upload all assets and generated JSON files from `scripts/data` here
 
 
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
-@click.argument("artist", ...)  # `AddressType`
+@click.argument("artist")  # `AddressType`
 def nft(network, artist):
     a = accounts.load(artist)
     piece = artist.deploy(project.ApePiece)
@@ -27,16 +29,4 @@ def nft(network, artist):
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
 def auction(network):
-    ... # `BuyApe.vy` deploy process to `network` here
-
-
-@cli.command()
-def merkle_root(filename):
-    TRUE_NODE = b"\x00" * 31 + b"\x01"
-    FALSE_NODE = b"\x00" * 32
-    addresses = Path(filename).read_text().split("\n")
-    tree = smt.SparseMerkleTree(20, FALSE_NODE)
-    for address in addresses:
-        tree.set(to_bytes(hexstr=address), TRUE_NODE)
-    return tree
-    # calculate merkle root with input file here
+    pass  # `BuyApe.vy` deploy process to `network` here
--- scripts/merkle.py
+++ scripts/merkle.py
@@ -1,34 +1,41 @@
+import json
+from pathlib import Path
 
 import click
 from ape import convert
+from ape.logging import logger
 from ape.types import AddressType
-from trie import smt
-from pathlib import Path
 from eth_utils import to_bytes, to_hex
-import json
+from trie import smt
 
-ADDRESS_DBFILE = Path.cwd()/"scripts/db.json"
+ADDRESS_DBFILE = Path.cwd() / "scripts/db.json"
 TRUE_NODE = b"\x00" * 31 + b"\x01"
 FALSE_NODE = b"\x00" * 32
 KEY_SIZE = 20
 
+
 def get_tree() -> smt.SparseMerkleTree:
     if ADDRESS_DBFILE.exists():
-        click.echo("Tree DB loading...")
+        logger.info("Tree DB loading...")
         db = json.loads(ADDRESS_DBFILE.read_text())
         return smt.SparseMerkleTree.from_db(
-            db={to_bytes(hexstr=k):to_bytes(hexstr=v) for k,v in db["db"].items()},
+            db={to_bytes(hexstr=k): to_bytes(hexstr=v) for k, v in db["db"].items()},
             root_hash=to_bytes(hexstr=db["root_hash"]),
             key_size=KEY_SIZE,
-            default=FALSE_NODE
+            default=FALSE_NODE,
         )
     else:
         return smt.SparseMerkleTree(key_size=KEY_SIZE, default=FALSE_NODE)
 
+
 def save_tree(tree):
-    click.echo("Saving Tree DB...")
-    db = {'db':{to_hex(k):to_hex(v) for k,v in tree.db.items()},'root_hash':to_hex(tree.root_hash)}
+    logger.info("Saving Tree DB...")
+    db = {
+        "db": {to_hex(k): to_hex(v) for k, v in tree.db.items()},
+        "root_hash": to_hex(tree.root_hash),
+    }
     ADDRESS_DBFILE.write_text(json.dumps(db))
+
 
 @click.group(short_help="Working with merkledb")
 def cli():
@@ -37,13 +44,15 @@ def cli():
     """
     pass
 
+
 @cli.command()
 def purge():
     """purge merkle db"""
     ADDRESS_DBFILE.unlink()
 
+
 @cli.command()
-@click.argument("addresses",nargs=-1)
+@click.argument("addresses", nargs=-1)
 def add(addresses):
     """add one or more addresses to the merkle db"""
     tree = get_tree()
@@ -51,8 +60,9 @@ def add(addresses):
         tree.set(to_bytes(hexstr=address), TRUE_NODE)
     save_tree(tree)
 
+
 @cli.command()
-@click.argument("addresses",nargs=-1)
+@click.argument("addresses", nargs=-1)
 def remove(addresses):
     """remove one or more addresses from the merkle db"""
     tree = get_tree()
@@ -60,7 +70,8 @@ def remove(addresses):
         tree.set(to_bytes(hexstr=address), FALSE_NODE)
     save_tree(tree)
 
-#import is a protected keyword - using a different function name
+
+# import is a protected keyword - using a different function name
 @cli.command(name="import")
 @click.argument("filename", type=click.Path(exists=True))
 def import_from_file(filename):
@@ -71,24 +82,32 @@ def import_from_file(filename):
         tree.set(to_bytes(hexstr=address), TRUE_NODE)
     save_tree(tree)
 
+
 @cli.command()
 @click.argument("address")
 def check(address):
     """check if address is in the merkle db"""
     tree = get_tree()
-    if tree.get(to_bytes(hexstr=convert(address,AddressType))) == TRUE_NODE:
+    if tree.get(to_bytes(hexstr=convert(address, AddressType))) == TRUE_NODE:
         click.echo(f"Address {address} exists in tree")
     else:
         click.echo(f"Address {address} DOES NOT exist in tree")
+
 
 @cli.command()
 @click.argument("address")
 def get_proof(address):
     """Get merkle proof for an address from the merkle db tree"""
     tree = get_tree()
-    if tree.get(to_bytes(hexstr=convert(address,AddressType))) == FALSE_NODE:
-        click.echo(f"Address {address} DOES NOT exist in tree")
+    if tree.get(to_bytes(hexstr=convert(address, AddressType))) == FALSE_NODE:
+        logger.error(f"Address {address} DOES NOT exist in tree")
         return
-    branch = tree.branch(to_bytes(hexstr=convert(address,AddressType)))
-    click.echo("\n".join(map(to_hex,branch)))
-    
+    branch = tree.branch(to_bytes(hexstr=convert(address, AddressType)))
+    click.echo("\n".join(map(to_hex, branch)))
+
+
+@cli.command()
+def get_root():
+    """Get merkle root from merkle db tree"""
+    tree = get_tree()
+    click.echo(to_hex(tree.root_hash))
