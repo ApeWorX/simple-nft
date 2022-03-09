09b9255f41fdb8c423fec0c91bafb4df4877ce98
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-03-09 14:18:30 -0800

    feat: added deployment

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,10 +2,8 @@ from pathlib import Path
 from readline import append_history_file
 
 import click
-from ape import accounts, project
-from ape.cli import NetworkBoundCommand, network_option
-from eth_utils import to_bytes
-from trie import smt
+from ape import accounts, project, logger
+from ape.cli import NetworkBoundCommand, network_option, account_option
 
 
 @click.group(short_help="Deploy the project")
@@ -13,20 +11,38 @@ def cli():
     pass
 
 
-@cli.command()
-def assets():
-    pass  # Upload all assets and generated JSON files from `scripts/data` here
-
-
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
+@account_option()
 @click.argument("artist")  # `AddressType`
-def nft(network, artist):
-    a = accounts.load(artist)
-    piece = artist.deploy(project.ApePiece)
+def nft(network, account, artist):
+    """Deploy the apepiece NFT contract"""
+    piece = account.deploy(project.ApePiece,artist,"")
+    click.echo(f"NFT deployed to {piece.address}")
 
 
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
-def auction(network):
-    pass  # `BuyApe.vy` deploy process to `network` here
+@account_option()
+@click.argument("nft")
+def auction(network,account,nft):
+    """`BuyApe.vy` deploy process to `network` here"""
+    nft = project.ApePiece.at(nft) #todo: get from dependencies
+    if nft.owner() != account:
+        logger.error("account is not owner of NFT")
+    auction = account.deploy(project.BuyApe,nft)
+    nft.setMinter(auction,sender=account)
+    click.echo(f"Auction deployed to {auction.address}")
+
+@cli.command(cls=NetworkBoundCommand)
+@network_option()
+@account_option()
+@click.argument("nft")
+def merkle(network,account):
+    """deploy merkle drop"""
+    nft = project.ApePiece.at(nft) #todo: get from dependencies
+    if nft.owner() != account:
+        logger.error("account is not owner of NFT")
+    merkle = account.deploy(project.MerkleDrop,nft,root_hash)
+    nft.setMinter(merkle,sender=account)
+    click.echo(f"MerkleDrop deployed to {merkle.address}")
